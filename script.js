52572137c6cee594f38d8f23ae008ef0599d96c8
sankit acharya <sankit.acharya@gmail.com>
2016-09-24 08:11:45 +0530

    funcation added

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 var input = document.getElementById('input');
 var operator = document.querySelectorAll('#operator div');
 var numbers = document.querySelectorAll('.numbers div');
+var result = document.getElementById('result');
 var currentval;
 var showresults = false
+
+var math_it_up = {
+    '÷':function(x,y){return x/y},
+    '×':function(x,y){return x*y},
+    '+':function(x,y){return x+y},
+    '-':function(x,y){return x-y}
+  };
+
+
+function calculate(array) {
+  var oper = ["÷","×","+","-"];
+
+
+  for(var i=0;i< oper.length;i++) {
+    for(var j =0;j< array.length;j++) {
+      if(array.length === 1) {
+        showresults = true;
+        return array[0];
+      }
+      if(Number.isFinite(j) === false) {
+        array.splice(j-1,3,toString(math_it_up[oper[i]](Number(array[j-1]),Number(array[j+1]))));
+        break;
+        break;
+      }
+    }
+  }
+}
 
 for(var i= 0;i <numbers.length;i++) {
   numbers[i].addEventListener('click',function(e){
@@ -10,6 +38,10 @@ for(var i= 0;i <numbers.length;i++) {
 
     last_elem = current_val[current_val.length -1];
 
+    if(e.target.innerHTML === "C"){
+      input.innerHTML = "";
+    }
+    else {
     if(showresults === false) {
       input.innerHTML += e.target.innerHTML;
     }
@@ -22,6 +54,7 @@ for(var i= 0;i <numbers.length;i++) {
       input.innerHTML = "";
       input.innerHTML += e.target.innerHTML;
     }
+  }
   });
 }
 
@@ -39,7 +72,17 @@ for(var i= 0;i <operator.length;i++) {
       alert("Please enter a number");
     }
     else {
-      input.innerHTML += e.target.innerHTML;
+      input.innerHTML += " "+e.target.innerHTML+" ";
     }
   });
 }
+
+result.addEventListener('click',function() {
+  var inputString = input.innerHTML;
+  var array = inputString.split(" ");
+  //var num = inputString.split(/\+|\-|\×|\÷/g);
+  //var oper = inputString.replace(/[0-9]|\./g, "").split("");
+  input.innerHTML = calculate(array);
+
+
+});
