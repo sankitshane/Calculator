6942cd19c6a6290dc40bf553bf1544572070478a
sankit acharya <sankit.acharya@gmail.com>
2016-09-25 02:33:42 +0530

    calc project complete

--- script.js
+++ script.js
@@ -9,17 +9,19 @@ var math_it_up = {
     '÷':function(x,y){return x/y},
     '×':function(x,y){return x*y},
     '+':function(x,y){return x+y},
-    '-':function(x,y){return x-y}
+    '-':function(x,y){return x-y},
+    'e':function(x){return x * 2.71828}
   };
 
 
 function calculate(array) {
-  var oper = ["÷","×","+","-"];
+  var oper = ["÷","×","+","-","e"];
   for(var i=0;i< oper.length;i++) {
     for(var j =1;j< array.length;j = j+2) {
 
       if(oper[i] === array[j]){
-         array.splice(j-1,3,math_it_up[oper[i]](Number(array[j-1]),Number(array[j+1])).toString());
+          if(oper[i] === 'e'){array.splice(j-1,2,math_it_up[oper[i]](Number(array[j-1])).toString());}
+          else {array.splice(j-1,3,math_it_up[oper[i]](Number(array[j-1]),Number(array[j+1])).toString());}
          if(array.length === 1) {
            showresults = true;
            input.innerHTML = array[0];
@@ -39,19 +41,22 @@ function calculate(array) {
 for(var i= 0;i <numbers.length;i++) {
   numbers[i].addEventListener('click',function(e){
     current_val = input.innerHTML;
-
+    console.log(current_val);
     last_elem = current_val[current_val.length -2];
 
     if(e.target.innerHTML === "C"){
       input.innerHTML = "";
     }
     else {
-    if(showresults === false) {
+    if(showresults === false && last_elem != 'e') {
       input.innerHTML += e.target.innerHTML;
     }
-    else if(showresults === true && (last_elem === "+" || last_elem === "-" || last_elem === "×" || last_elem === "÷" || last_elem === "e")) {
+    else if(showresults === true && (last_elem === "+" || last_elem === "-" || last_elem === "×" || last_elem === "÷")) {
       showresults = false;
       input.innerHTML += e.target.innerHTML;
+    }
+    else if(showresults === false && last_elem === 'e') {
+      input.innerHTML += ' × ' + e.target.innerHTML;
     }
     else {
       showresults = false;
@@ -65,11 +70,10 @@ for(var i= 0;i <numbers.length;i++) {
 for(var i= 0;i <operator.length;i++) {
   operator[i].addEventListener('click',function(e){
     current_val = input.innerHTML;
-
-    last_elem = current_val[current_val.length -1];
+    last_elem = current_val[current_val.length -2];
 
     if(last_elem === "+" || last_elem === "-" || last_elem === "×" || last_elem === "÷" || last_elem === "e") {
-      var newString = current_val.substring(0,current_val.length-1) + e.target.innerHTML;
+      var newString = current_val.substring(0,current_val.length-2) + e.target.innerHTML + " ";
       input.innerHTML = newString;
     }
     else if(current_val === ""){
@@ -84,5 +88,10 @@ for(var i= 0;i <operator.length;i++) {
 result.addEventListener('click',function() {
   var inputString = input.innerHTML;
   var array = inputString.split(" ");
+  for(var i =0 ; i < array.length ; i++) {
+    if(array[i] === "")
+      array.splice(i,1);
+  }
+  console.log(array);
   calculate(array);
 });
